5851cb26a17a083c4eb01748baa1e1d56899ff31
thesmartwon <thesmartwon@pm.me>
2024-10-24 11:16:52 -0400

    use 3 letter i18n codes, use deno

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,18 @@
 //! These are the enabled languages.
-import * as i18n from "@solid-primitives/i18n";
-import en from './i18n/en-US.json';
+import * as i18n from '@solid-primitives/i18n';
+// TODO: remove this as default. ship default from server based on client HTTP header
+import en from './i18n/eng.json';
 
-export const languages = ['en-US'] as const;
+// Browsers use https://datatracker.ietf.org/doc/html/rfc5646#section-2.2.1
+// We use https://www.loc.gov/standards/iso639-2/php/code_list.php
+const langs = {
+	"en": "eng",
+	"es": "spa",
+	"he": "heb",
+} as const;
+
+export type Language = typeof langs[keyof typeof langs];
+export const languages = Object.values(langs) as Language[];
 export const defaultDict = en;
 
 export type Locale = typeof languages[number];
@@ -10,11 +20,14 @@ export type RawDictionary = typeof en;
 export type Dictionary = i18n.Flatten<RawDictionary>;
 
 export async function fetchTranslator(locale: Locale) {
-  const dict: RawDictionary = await import(`./i18n/${locale}.json`);
+	const dict: RawDictionary = await import(`./i18n/${locale}.json`);
 	return i18n.translator(() => dict);
 }
 
 export function navigatorLang(): Locale {
-	const prefix = navigator.language?.split('-')[0];
-	return languages.find(l => l == navigator.language || l.startsWith(prefix)) || 'en-US';
+	const alpha = navigator.language?.split('-')[0];
+	// Spec allows 3 letter codes.
+	if (languages.includes(alpha as Language)) return alpha as Language;
+
+	return langs[alpha as keyof typeof langs] ?? 'eng';
 }
