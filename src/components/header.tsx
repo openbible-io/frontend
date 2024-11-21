122899a4c6a5c9e426aae7ee4cc34ef3dd6687a4
thesmartwon <thesmartwon@pm.me>
2024-11-21 11:25:04 -0500

    responsive drawer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,16 +3,37 @@ import Button from "./button.tsx";
 import Settings from "./settings.tsx";
 import * as store from "../stores/client.ts";
 import { useStore } from "@nanostores/preact";
+import { useEffect, useRef, useState } from "preact/hooks";
+//import resolveConfig from 'tailwindcss/resolveConfig';
+//
+//const fullConfig = resolveConfig({ content: [] });
+//const breakpoints = (Object.entries(fullConfig.theme.screens)
+//	.map(([k, v]) => [k, parseInt(v)]) as [string, number][])
+//	.sort(([_, v1], [__, v2]) => v2 - v1);
+//
+//function getCurrentBreakpoints() {
+//    return breakpoints.find(([_, v]) => globalThis.innerWidth > v)?.[0];
+//}
 
 export const i18n = store.i18n("header", {
 	settings: "Settings",
 });
 
 export default function Header(props: { class?: string }) {
+	const ref = useRef<HTMLElement>(null);
 	const t9n = useStore(i18n);
+	const [height, setHeight] = useState("0");
+
+	useEffect(() => {
+		setHeight(getComputedStyle(ref.current!).height);
+	}, []);
 
 	return (
-		<header class={`grid grid-cols-6 w-full p-2 ${props.class}`}>
+		<header
+			ref={ref}
+			class={`grid grid-cols-6 w-full p-2 ${props.class}`}
+			style={`--height-header:${height}`}
+		>
 			<div
 				class={classnames(
 					"col-start-2 col-span-4",
@@ -34,16 +55,9 @@ export default function Header(props: { class?: string }) {
 			<div
 				id="drawer"
 				popover
-				onBeforeToggle={(ev: ToggleEvent) => {
-					const t = ev.target as HTMLDivElement;
-					console.log(ev.target);
-					t.style.setProperty(
-						"--top",
-						t.parentElement!.getBoundingClientRect().bottom + "px",
-					);
-				}}
 				class={classnames(
-					"m-0 p-2 w-1/3",
+					"m-0 p-2 w-screen md:w-96",
+					"[--top:50vh] md:[--top:var(--height-header)]",
 					"absolute ltr:left-auto ltr:right-0 rtl:right-auto rtl:left-0",
 				)}
 			>
